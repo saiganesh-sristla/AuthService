e36b6fbcc371fb5e4f01c2d8f850db40e49a8cdb
saiganesh-sristla <saiganesh3108@gmail.com>
2024-06-03 18:25:32 +0530

    Added functions to create and verify jwt token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,7 @@
+const { JWT_KEY } = require('../config/serverConfig');
 const UserRepository = require('../repository/user-repository');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 class UserService{
     constructor(){
         this.userRepository = new UserRepository();
@@ -11,6 +13,36 @@ class UserService{
             return user;
         } catch (error) {
             console.log("something went wrong at service layer");
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try {
+            const token = jwt.sign(user, JWT_KEY, {expiresIn: "1d"})
+            return token;
+        } catch (error) {
+            console.log("something went wrong creating token");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("something went wrong verifying the token");
+            throw error;
+        }
+    }
+
+    comparePassword(plainPassword, hashPassword){
+        try {
+            const response = bcrypt.compareSync(plainPassword, hashPassword);
+            return response;
+        } catch (error) {
+            console.log("something went wrong comparing password");
             throw error;
         }
     }
